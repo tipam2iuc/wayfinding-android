c7ce3d1021a367623edbc8eb2ed98864491af6e4
rigole <foplacide@gmail.com>
2019-03-11 02:13:24 +0100

    Update MyPagerAdapter.java

--- app/src/main/java/e/plass/introslide/MyPagerAdapter.java
+++ app/src/main/java/e/plass/introslide/MyPagerAdapter.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * Created by NgocTri on 11/29/2017.
+ * Created by Rigole on 02/29/2019.
  */
 
 public class MyPagerAdapter extends PagerAdapter {
