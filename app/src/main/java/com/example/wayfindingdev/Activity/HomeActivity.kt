3dfed6f8e579bce9d430ee17cae5802c94849509
Placide FOLEU <foplacide@gmail.com>
2019-06-18 10:24:44 +0100

    Drawer ajouté
    r

--- app/src/main/java/com/example/wayfindingdev/Activity/HomeActivity.kt
+++ app/src/main/java/com/example/wayfindingdev/Activity/HomeActivity.kt
@@ -5,8 +5,15 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,10 +22,12 @@ import com.bumptech.glide.Glide
 import com.example.wayfindingdev.Fragment.DomaineFragment
 import com.example.wayfindingdev.Fragment.HomeFragment
 import com.example.wayfindingdev.R
+import com.example.wayfindingdev.R.id.toolbar
 import com.example.wayfindingdev.Tools.currentUser
+import kotlinx.android.synthetic.main.activity_home.*
 
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     private lateinit var dialog:Dialog
     private lateinit var closeMsg:ImageButton
     private lateinit var msgText:TextView
@@ -32,6 +41,16 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+       val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navview  : NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.map, R.string.map
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navview.setNavigationItemSelectedListener(this)
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView_home)
         imageView = findViewById(R.id.profile_image)
@@ -92,4 +111,38 @@ class HomeActivity : AppCompatActivity() {
 
 
     }
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.toolbar -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+
+
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
 }
+
